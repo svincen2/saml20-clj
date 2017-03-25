225f081355b195a994cdbfebec53a0b4581e7c1d
Kenji Nakamura <kenji@signifier.jp>
2017-03-25 13:32:49 +0900

    Released 0.1.7.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.11.0"]
                  [ring "1.4.0"]
