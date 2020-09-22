55cc9ba88f2fb473f44d18262fa1ef31aa9c8c96
Cam Saul <github@camsaul.com>
2020-09-22 13:45:06 -0700

    Fix Clojars deploy

--- project.clj
+++ project.clj
@@ -75,5 +75,5 @@
   [["clojars"
     {:url           "https://clojars.org/repo"
      :username      :env/clojars_username
-     :password      :env/clojars_password
+     :password      :env/clojars_deploy_token
      :sign-releases false}]])
