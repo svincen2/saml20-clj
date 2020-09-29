9f185eaaf365b4b66a84f8930873f6366c5d76e4
Cam Saul <github@camsaul.com>
2020-09-29 13:28:00 -0700

    2.0.0-alpha2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metabase/saml20-clj "2.0.0-alpha2-SNAPSHOT"
+(defproject metabase/saml20-clj "2.0.0-alpha2"
   :description "Improved SAML 2.0 library for SSO."
   :url "https://github.com/metabase/saml20-clj"
   :min-lein-version "2.5.0"
